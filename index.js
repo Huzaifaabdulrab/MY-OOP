29e86f56b55038e35b69c386004d10ac30c1d910
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2024-07-28 18:23:44 +0500

    OOP-PROJECT

---
+++ index.js
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+//Student class
+class Student {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+//Person class
+class Person {
+    students = [];
+    addStudent(student) {
+        this.students.push(student);
+    }
+}
+let programerName = ">>>>>>>>>>>>>>>>>>>>HUZAIFA ABDULRAB<<<<<<<<<<<<<<<<<<<<<<<<<";
+console.log(chalk.underline.blueBright(programerName));
+//inherit Perosn
+const newPerson = new Person();
+//async function
+const programStart = async (newPerson) => {
+    do {
+        //user input
+        const answer = await inquirer.prompt({
+            name: "userAnswer",
+            type: "list",
+            message: "Who would you like to talk to?",
+            choices: ["Self", "Student", "Exit"] //choices
+        });
+        if (answer.userAnswer == "Self") {
+            console.log(chalk.yellowBright("Hello, I am talking to myself..."));
+            console.log(chalk.yellowBright("I am fine..."));
+        }
+        else if (answer.userAnswer == "Student") {
+            let validStdName = false;
+            let studentNameInput = "";
+            while (!validStdName) {
+                const studentAnswer = await inquirer.prompt({
+                    name: "student",
+                    type: "input",
+                    message: "Which student would you like to talk to?"
+                });
+                if (isNaN(Number(studentAnswer.student))) {
+                    studentNameInput = studentAnswer.student;
+                    validStdName = true;
+                }
+                else {
+                    console.log(chalk.red(`${studentAnswer.student} Please enter a valid name, not a number.`));
+                }
+            }
+            const studentName = newPerson.students.find(student => student.name == studentNameInput);
+            if (!studentName) {
+                const newStudent = new Student(studentNameInput);
+                newPerson.addStudent(newStudent);
+                console.log(chalk.green(`Hello, I am ${newStudent.name} and I am fine.`));
+                console.log(newPerson.students);
+            }
+            else {
+                console.log(chalk.green(`Hello, I am ${studentName.name} and I am fine.`));
+                console.log(newPerson.students);
+            }
+        }
+        else if (answer.userAnswer == "Exit") {
+            console.log(chalk.blue.bold(">>>>>>>>>>>>>>>>>>>>>>Thank you>>>>>>>>>>>>>>>>>>>>>>"));
+            break;
+        }
+    } while (true);
+};
+programStart(newPerson); //Function call
